5d2d111331c9d4230d46147e8b1033d90df0a6da
Crowdbotics <team@crowdbotics.com>
2020-09-19 21:16:05 +0000

    Update models

--- backend/find_a_roping_20455/wsgi.py
+++ backend/find_a_roping_20455/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'find_a_roping_20455.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "find_a_roping_20455.settings")
 
 application = get_wsgi_application()
